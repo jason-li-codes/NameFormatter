cfe1b36ff912fa77f5a97b51978efdeff056d2a8
lijas <lijason991@gmail.com>
2025-10-22 20:50:00 -0400

    first commit

---
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,90 @@
+package com.pluralsight;
+
+public class NameFormatter {
+
+    private NameFormatter() {}
+
+    private static String properCasing(String namePart) {
+        return Character.toUpperCase(namePart.charAt(0)) + namePart.substring(1).toLowerCase();
+    }
+
+    public static String format(String firstName, String lastName) {
+        return NameFormatter.properCasing(lastName) + ", " + NameFormatter.properCasing(firstName);
+    }
+
+    public static String format(String prefix, String firstName,
+                                String middleName, String lastName, String suffix) {
+        return String.join(", ", NameFormatter.properCasing(lastName),
+                String.join(" ", NameFormatter.properCasing(prefix), NameFormatter.properCasing(firstName),
+                        NameFormatter.properCasing(middleName)), suffix);
+    }
+
+    public static String format(String fullName) {
+        String[] nameParts = fullName.split(" ");
+        String prefix = "";
+        String firstName = "";
+        String middleName = "";
+        String lastName = "";
+        String suffix = "";
+
+        switch (nameParts.length) {
+            case 2:
+                firstName = nameParts[0].trim();
+                lastName = nameParts[1].trim();
+                break;
+            case 3:
+                if (nameParts[0].contains(".")) {
+                    prefix = nameParts[0].trim();
+                    firstName = nameParts[1].trim();
+                    lastName = nameParts[2].trim();
+                } else if (nameParts[1].contains(",")) {
+                    firstName = nameParts[0].trim();
+                    lastName = nameParts[1].trim();
+                    suffix = nameParts[2].trim();
+                } else {
+                    firstName = nameParts[0].trim();
+                    middleName = nameParts[1].trim();
+                    lastName = nameParts[2].trim();
+                }
+                break;
+            case 4:
+                if (!nameParts[0].contains(".")) {
+                    firstName = nameParts[0].trim();
+                    middleName = nameParts[1].trim();
+                    lastName = nameParts[2].trim();
+                    suffix = nameParts[3].trim();
+                } else if (!nameParts[2].contains(",")) {
+                    prefix = nameParts[0].trim();
+                    firstName = nameParts[1].trim();
+                    middleName = nameParts[2].trim();
+                    lastName = nameParts[3].trim();
+                } else {
+                    prefix = nameParts[0].trim();
+                    firstName = nameParts[1].trim();
+                    lastName = nameParts[2].trim();
+                    suffix = nameParts[3].trim();
+                }
+                break;
+            case 5:
+                prefix = nameParts[0].trim();
+                firstName = nameParts[1].trim();
+                middleName = nameParts[2].trim();
+                lastName = nameParts[3].trim();
+                suffix = nameParts[4].trim();
+                break;
+        }
+        String formattedName = NameFormatter.properCasing(lastName).replace(",", "") + ", ";
+        if (!prefix.isEmpty()) {
+            formattedName += NameFormatter.properCasing(prefix).replace(".", "") + ". ";
+        }
+        formattedName += NameFormatter.properCasing(firstName) + " ";
+        if (!middleName.isEmpty()) {
+            formattedName += NameFormatter.properCasing(middleName);
+        }
+        if (!suffix.isEmpty()) {
+            formattedName += ", " + suffix;
+        }
+        return formattedName;
+    }
+
+}
